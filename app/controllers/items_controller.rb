5e2771988759dfebdda593ad4a44e70e86eb072a
中平 慶春 <nakadaira@nakahiranoMacBook-Pro-2.local>
2021-08-04 12:53:18 +0900

    商品出品ページの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user! 
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else 
+      render action: :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:product_name, :price, :description, :category_id, :postage_id, :status_id, :shipment_id, :estimated_shipping_id ).merge(user_id: current_user.id)
+  end
+
 end
