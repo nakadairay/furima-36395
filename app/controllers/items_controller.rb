b668f864cd17c95956679109eb9732f959fa143d
中平 慶春 <nakadaira@nakahiranoMacBook-Pro-2.local>
2021-08-05 17:02:01 +0900

    items_controllerでのbefore_actionの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit, :show, :update]
+  before_action :not_user_move_index, only: [:edit, :update]
   before_action :authenticate_user!, except: [:index, :show]
+
   def index
     @item = Item.order('created_at DESC')
   end
@@ -18,16 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-    redirect_to root_path unless current_user.id == @item.user_id
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -40,5 +39,13 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :product_name, :price, :description, :category_id, :postage_id, :status_id, :shipment_id,
                                  :estimated_shipping_id).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def not_user_move_index
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 end
