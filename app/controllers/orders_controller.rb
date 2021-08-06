ee4210d86b6f6617780ee18f16dc0c5cbae33ca5
中平 慶春 <nakadaira@nakahiranoMacBook-Pro-2.local>
2021-08-06 18:09:44 +0900

    order_addressの修正　バリデーションの追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,7 +5,22 @@ class OrdersController < ApplicationController
     @order_address = OrderAddress.new
   end
 
-  def create
 
+  def create
+    @items = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      return redirect_to root_path
+    else
+      render :index
+    end
   end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postcode, :shipment_id, :city, :block, :building, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id)
+  end
+
 end
