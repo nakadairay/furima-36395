e615e4d205aba75ccb3519158ea313104f9fcc2b
中平 慶春 <nakadaira@nakahiranoMacBook-Pro-2.local>
2021-08-12 16:03:14 +0900

    マイページの下地作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def update
+    binding.pry
+    @user = User.find(params[:id])
+    if @user.update(user_params) 
+      redirect_to root_path 
+    else
+      redirect_to action: "show" 
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :email,)
+  end
+end
